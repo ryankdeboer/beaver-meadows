c198bf288ab99719f8ef2fb832f457d0750e658f
Ryan DeBoer <ryandeboer@ryans-mbp-2.lan>
2023-07-06 10:14:13 -0500

    Updated mens and womens week

--- frontend/src/pages/MensLeaguePage.jsx
+++ frontend/src/pages/MensLeaguePage.jsx
@@ -208,7 +208,7 @@ function MensLeaguePage() {
                             <td className="whitespace-nowrap px-3 py-4"></td>
                             <td className="whitespace-nowrap px-3 py-4">12 v 14</td>
                           </tr>
-                          <tr className="border-b dark:border-neutral-500 bg-zinc-100 dark:bg-zinc-800">
+                          <tr className="border-b dark:border-neutral-500">
                             <td className="whitespace-nowrap px-3 py-4 w-10 font-medium">Week 9</td>
                             <td className="whitespace-nowrap px-3 py-4">2 v 4</td>
                             <td className="whitespace-nowrap px-3 py-4">1 v 13</td>
@@ -220,7 +220,7 @@ function MensLeaguePage() {
                             <td className="whitespace-nowrap px-3 py-4"></td>
                             <td className="whitespace-nowrap px-3 py-4">3 v 7</td>
                           </tr>
-                          <tr className="border-b dark:border-neutral-500">
+                          <tr className="border-b dark:border-neutral-500 bg-zinc-100 dark:bg-zinc-800">
                             <td className="whitespace-nowrap px-3 py-4 w-10 font-medium">Week 10</td>
                             <td className="whitespace-nowrap px-3 py-4">6 v 14</td>
                             <td className="whitespace-nowrap px-3 py-4">4 v 11</td>
